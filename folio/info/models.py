30ce257d2aa71048705edb70bdc80f7bcda99daf
Ankit Kumar <nasaankit01@gmail.com>
2023-06-08 21:41:03 +0530

    model created Skill & AboutMe
    Created model skill for skills and AboutMe in info app.

--- folio/info/models.py
+++ folio/info/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# aboutme model here
+
+class Skill(models.Model):
+    skill_name = models.CharField(blank=False,max_length=50)
+    about = models.TextField(blank=True)
+    icon = models.FileField(upload_to='skill_icons/',null=True,blank=True)
+
+    def __str__(self):
+        return str(self.skill_name)
+
+class AboutMe(models.Model):
+    name = models.CharField(blank=False,max_length=50)
+    profile_image = models.FileField(upload_to='profile_images/',null=True,blank=True)
+    who_am_i = models.TextField(blank=True,null=True)
+    what_can_i_do = models.TextField(blank=True,null=True)
+    skills = models.ManyToManyField(Skill)
+    resume = models.FileField(upload_to='resume/',null=True,blank=True)
+    hobbies = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return str(self.name)

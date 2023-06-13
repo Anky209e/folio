52011eb93f1f730ec23c6f2ff4be27848f7dc8e1
Ankit Kumar <nasaankit01@gmail.com>
2023-06-13 22:07:27 +0530

    panel cutomization

--- folio/work/admin.py
+++ folio/work/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from  work.models import *
 
-admin.site.register(Experience)
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ("company_name","post_name")
